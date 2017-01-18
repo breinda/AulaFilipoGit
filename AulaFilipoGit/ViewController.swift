26644ea6a7ba47ceab98590f6d6e9db4ec32ee7d
Brenda <brendac@live.it>
2017-01-18 14:55:28 -0200

    Initial Commit

---
+++ AulaFilipoGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AulaFilipoGit
+//
+//  Created by Brenda Carrocino on 18/01/17.
+//  Copyright © 2017 Brenda Carrocino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
